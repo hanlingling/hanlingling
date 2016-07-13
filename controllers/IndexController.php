0568440a7730f46a68f2588ff79f1b04061e31f9
hll Name <909716893@example.com>
2016-07-14 07:36:21 +0800

    第一次上传项目

---
+++ controllers/IndexController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+class IndexController extends Controller
+{
+    public $layout="menu.php";
+    public $enableCsrfValidation = false;
+    public function actionLsts(){
+        //查询他是否有登录
+        $session = \Yii::$app->session;
+        $session->open();
+        $re=$session->get('uname');
+        if($re){
+            return  $this->render("lsts");
+        }else{
+            return $this->redirect("index.php?r=login/index");
+        }
+
+    }
+    //IP展示
+    public function actionIp(){
+        $arr=\Yii::$app->db->createCommand("select * from we_ip")->queryAll();
+        return $this->render("ip",['arr'=>$arr]);
+    }
+    //ip删除
+    public function actionDel(){
+        $id=\Yii::$app->request->get("id");
+        $arr=\Yii::$app->db->createCommand()->delete("we_ip",['iid'=>$id])->execute();
+        if($arr){
+            return $this->redirect("index.php?r=index/ip");
+        }else{
+            echo "删除失败";
+        }
+    }
+    public function actionRemove(){
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('uname');
+        return $this->redirect("index.php?r=index/lsts");
+    }
+    //ip展示
+    public function actionAdd(){
+        if(empty($_POST)){
+            return $this->render("insert");
+        }else{
+            $session = \Yii::$app->session;
+            $session->open();
+            $re=$session->get('uname');
+            $ip=\Yii::$app->request->post("ip");
+            $res=\Yii::$app->db->createCommand("select * from we_ip where iip='$ip' and iuser='$re'")->queryAll();
+            if($res){
+                echo 1;
+            }else{
+                $arr=\Yii::$app->db->createCommand()->insert("we_ip",['iip'=>$ip,"iuser"=>$re])->execute();
+                if($arr){
+                    echo 2;
+                }
+            }
+
+        }
+    }
+    /*
+     * 添加公众号*/
+   public function actionNumadd()
+   {
+       if(empty($_POST)){
+           return $this->render("publicnum");
+       }else{
+           $session = \Yii::$app->session;
+           $session->open();
+           $re=$session->get('uname');
+           $num=\Yii::$app->request->post("num");
+           $res=\Yii::$app->db->createCommand("select * from we_publicnum where num='$num' and iuser='$re'")->queryAll();
+           if($res){
+               echo 1;
+           }else{
+               $arr=\Yii::$app->db->createCommand()->insert("we_publicnum",['num'=>$num,"iuser"=>$re])->execute();
+               if($arr){
+                   echo 2;
+               }
+           }
+
+       }
+   }
+}
---
+++ views/login/index.php
@@ -0,0 +1,71 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>欢迎微09A</title>
+
+    <!-- For-Mobile-Apps -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Welcome Back Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //For-Mobile-Apps -->
+
+    <!-- Style --> <link rel="stylesheet" href="./assets/css/style.css" type="text/css" media="all" />
+
+    <!-- Web-Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,500,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <!-- //Web-Fonts -->
+
+</head>
+<body>
+
+
+
+<div class="container">
+
+    <div class="image">
+        <img src="./assets/img/10.jpg" alt="Welcome">
+    </div>
+    <!--<h2>Welcome</h2>-->
+    <h2 style="color:black">欢迎微9A</h2>
+    <h3>Login</h3>
+
+    <div class="signin">
+        <form  action="index.php?r=login/lst" method="post">
+            <input type="text" class="user" placeholder="请输入账号" required="" name="name">
+            <input type="password" class="pass" placeholder="请输入密码" required="" name="pwd">
+            <input type="submit" value="登陆" />
+            <ul>
+                <li>
+                    <input type="checkbox" id="brand1" value="">
+                    <label for="brand1"><span></span>记住我</label>
+                </li>
+            </ul>
+        </form>
+    </div>
+    <br>
+    <div class="social">
+        <ul class="social-icons slideanim">
+            <li><a href="#" class="facebook" title="Go to Our Facebook Page"></a></li>
+            <li><a href="#" class="twitter" title="Go to Our Twitter Account"></a></li>
+            <li><a href="#" class="googleplus" title="Go to Our Google Plus Account"></a></li>
+        </ul>
+    </div>
+
+</div>
+
+<div class="footer">
+    <p> &copy; 2016 Welcome Back Login Form. All Rights Reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+</div>
+
+</body>
+</html>
